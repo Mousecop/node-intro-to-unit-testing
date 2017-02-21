a5e0fdef8fb8f6555eaa4d82709d3193a2bb3fe8
Mousecop <joaskew13@gmail.com>
2017-02-21 11:46:28 -0500

    made more descriptive test cases

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -4,8 +4,10 @@ const fizzBuzz = require('../fizzBuzzer');
 
 
 describe('Fizz Buzz', function() {
-    it('should print FizzBuzz', function() {
-        fizzBuzz(15).should.equal('fizz-buzz');
+    it('should print FizzBuzz when divisbile by 3 and 5', function() {
+        [15, 30, 45].forEach(function(item) {
+            fizzBuzz(item).should.equal('fizz-buzz');
+        });
     });
     it('should print Buzz', function() {
         fizzBuzz(5).should.equal('buzz');
