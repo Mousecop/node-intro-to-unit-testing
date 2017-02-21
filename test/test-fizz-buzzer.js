7ade359785e973e7973ef254b03ecbb1c717e3c9
Mousecop <joaskew13@gmail.com>
2017-02-21 11:36:15 -0500

    Made test for fizzBuzz func

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -1,3 +1,20 @@
 const should = require('chai').should();
 
 const fizzBuzz = require('../fizzBuzzer');
+
+
+describe('Fizz Buzz', function() {
+    it('should print FizzBuzz', function() {
+        fizzBuzz(15).should.equal('fizz-buzz');
+    });
+    it('should print Buzz', function() {
+        fizzBuzz(5).should.equal('buzz');
+    });
+    it('should print Fizz', function() {
+        fizzBuzz(3).should.equal('fizz');
+    });
+    // describe('Edge Case', function() {
+    //     it('should test against string inputs', function() {
+    //         fizzBuzz('abc').should.
+    //     });
+});
